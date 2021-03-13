393247ac2984aaba6602c4db32140ff522e498c1
thetai3110 <trthetai3110@gmail.com>
2021-03-13 16:20:44 +0700

    ready

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,4 @@
 module.exports = {
-    serverName: 'https://blog-something-api.herokuapp.com/',
+    serverName: 'https://blog-something-api.herokuapp.com',
     mongodb: 'mongodb+srv://thetai:tailaanh@cluster0.yxf8m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
